500383befbdda2e31e8543dcf90e8141adca7439
WAHU0508 <sharongikenye@gmail.com>
2024-07-22 17:38:07 +0300

    Using employee record as context

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
 /* Your Code Here */
+// Function to create an employee record
+const createEmployeeRecord = function (employeeRecord) {
+    return {
+        firstName: employeeRecord[0],
+        familyName: employeeRecord[1],
+        title: employeeRecord[2],
+        payPerHour: employeeRecord[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+};
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+// Function to create multiple employee records
+const createEmployeeRecords = function (employeeRecords) {
+    return employeeRecords.map(createEmployeeRecord);
+};
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+// Define methods separately
+const createTimeInEvent = function (timeStamp) {
+    const [date, hour] = timeStamp.split(' ');
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour, 10),
+        date: date
+    });
+    return this;
+};
+
+const createTimeOutEvent = function (timeStamp) {
+    const [date, hour] = timeStamp.split(' ');
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour, 10),
+        date: date
+    });
+    return this;
+};
+
+const hoursWorkedOnDate = function (date) {
+    let timeIn = this.timeInEvents.find(e => e.date === date);
+    let timeOut = this.timeOutEvents.find(e => e.date === date);
+    if (!timeIn) { throw new Error(`No timeIn event found for date: ${date}`); }
+    if (!timeOut) { throw new Error(`No timeOut event found for date: ${date}`); }
+
+    const timeInDate = timeIn.hour;
+    const timeOutDate = timeOut.hour;
+
+    const hoursWorked = (timeOutDate - timeInDate) / 100;
+    if (hoursWorked < 0) throw new Error(`Invalid time range for date: ${date}`);
+    return hoursWorked;
+};
+
+const wagesEarnedOnDate = function (date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    return hoursWorked * this.payPerHour;
+};
+
+const calculatePayroll = function (employeeRecords) {
+    return employeeRecords.reduce((total, employee) => {
+        return total + allWagesFor.call(employee);
+    }, 0);
+};
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -20,4 +73,24 @@ const allWagesFor = function () {
 
     return payable
 }
+
+const findEmployeeByFirstName = function(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+}
+
+let bpRecord = createEmployeeRecord(["Byron", "Poodle", "Mascot", 3]);
+let date1 = "2014-02-28 1400";
+let date2 = "2014-02-28 1700";
+
+// Use `call` to ensure the correct context
+createTimeInEvent.call(bpRecord, date1);
+createTimeOutEvent.call(bpRecord, date2);
+
+let hoursWorked = hoursWorkedOnDate.call(bpRecord, "2014-02-28");
+let wagesEarned = wagesEarnedOnDate.call(bpRecord, "2014-02-28");
+let totalWages = allWagesFor.call(bpRecord);
+
+console.log(hoursWorked); 
+console.log(wagesEarned);   
+console.log(totalWages);  
 
